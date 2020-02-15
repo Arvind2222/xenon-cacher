1935d3980545fb8472a68cc24cb5733bd5c88a2e
Merlintor <merlinfuchs2001@gmail.com>
2020-02-15 13:42:38 +0100

    Bulk write operations to increase performance
    Db writes happen every 10s or when the max_bulk size is reached. This makes it possible for mongodb to process multiple operations at the same time. The order doesn't really matter for us.

--- cacher.py
+++ cacher.py
@@ -2,8 +2,10 @@ import aio_pika
 import asyncio
 import ujson as json
 import traceback
+from pymongo import UpdateOne, DeleteOne, DeleteMany
 from motor.motor_asyncio import AsyncIOMotorClient
 import pymongo
+import time
 
 
 class Cacher:
@@ -13,9 +15,47 @@ class Cacher:
 
         self.r_con = None
         self.r_channel = None
+        self.write_lock = asyncio.Lock()
+        self.last_write = time.perf_counter()
+        self.max_bulk = 500
+        self.bulk_size = 0
+        self.bulk = {}
 
         self.loop = loop or asyncio.get_event_loop()
         self.db = AsyncIOMotorClient(host=mongo_url).cache
+
+    async def write_bulk(self):
+        self.bulk_size = 0
+        self.last_write = time.perf_counter()
+        for col, bulk in self.bulk.items():
+            operations = [o for _, o in bulk]
+            self.bulk[col] = []
+            if len(operations) > 0:
+                await getattr(self.db, col).bulk_write(operations, ordered=False)
+
+            for msg, _ in bulk:
+                if not msg.processed:
+                    await msg.ack()  # Acknowledge Message
+
+    async def write_task(self):
+        while True:
+            await asyncio.sleep(10)
+            if (time.perf_counter() - self.last_write) <= 10:
+                # Last write was pretty recent
+                continue
+
+            if self.bulk_size > 0:
+                await self.write_lock.acquire()
+                try:
+                    # Check again to not start a unnecessary write
+                    if self.bulk_size > 0:
+                        await self.write_bulk()
+
+                except:
+                    traceback.print_exc()
+
+                finally:
+                    self.write_lock.release()
 
     async def _message_received(self, msg):
         payload = json.loads(msg.body)
@@ -27,9 +67,24 @@ class Cacher:
             pass
 
         else:
-            await func(data)
+            for col, operation in func(data):
+                self.bulk_size += 1
+                if col not in self.bulk:
+                    self.bulk[col] = [(msg, operation)]
 
-    async def cache_guild_create(self, data):
+                else:
+                    self.bulk[col].append((msg, operation))
+
+        if self.bulk_size >= self.max_bulk:
+            await self.write_lock.acquire()
+            try:
+                # Check again to not start a unnecessary write
+                if self.bulk_size > self.max_bulk:
+                    await self.write_bulk()
+            finally:
+                self.write_lock.release()
+
+    def cache_guild_create(self, data):
         ignore = ("emojis", "voice_states", "presences")
         for k in ignore:
             data.pop(k, None)
@@ -40,76 +95,76 @@ class Cacher:
         for role in roles:
             role["_id"] = role["id"]
             role["guild_id"] = guild_id
-            await self.db.roles.update_one({"_id": role["id"]}, {"$set": role}, upsert=True)
+            yield "roles", UpdateOne({"_id": role["id"]}, {"$set": role}, upsert=True)
 
         channels = data.pop("channels", [])
         for channel in channels:
             channel["_id"] = channel["id"]
             channel["guild_id"] = guild_id
-            await self.db.channels.update_one({"_id": channel["id"]}, {"$set": channel}, upsert=True)
+            yield "channels", UpdateOne({"_id": channel["id"]}, {"$set": channel}, upsert=True)
 
         members = data.pop("members", [])
         for member in members:
             member["guild_id"] = guild_id
-            await self.db.members.update_one({
+            yield "members", UpdateOne({
                 # user.id and guild_id should be a unique compound index
                 "id": member["user"]["id"],
                 "guild_id": guild_id
             }, {"$set": member}, upsert=True)
 
         data["_id"] = guild_id
-        await self.db.guilds.update_one({"_id": guild_id}, {"$set": data}, upsert=True)
+        yield "guilds", UpdateOne({"_id": guild_id}, {"$set": data}, upsert=True)
 
     def cache_guild_update(self, data):
-        return self.cache_guild_create(data)
+        yield from self.cache_guild_create(data)
 
-    async def cache_guild_delete(self, data):
+    def cache_guild_delete(self, data):
         guild_id = data["id"]
-        await self.db.guilds.delete_one({"_id": guild_id})
-        await self.db.members.delete_many({"guild_id": guild_id})
-        await self.db.channels.delete_many({"guild_id": guild_id})
-        await self.db.roles.delete_many({"guild_id": guild_id})
+        yield "guilds", DeleteOne({"_id": guild_id})
+        yield "members", DeleteMany({"guild_id": guild_id})
+        yield "channels", DeleteMany({"guild_id": guild_id})
+        yield "roles", DeleteMany({"guild_id": guild_id})
 
-    async def cache_channel_create(self, data):
+    def cache_channel_create(self, data):
         data["_id"] = data["id"]
-        await self.db.channels.update_one({"_id": data["id"]}, {"$set": data}, upsert=True)
+        yield "channels", UpdateOne({"_id": data["id"]}, {"$set": data}, upsert=True)
 
     def cache_channel_update(self, data):
-        return self.cache_channel_create(data)
+        yield from self.cache_channel_create(data)
 
-    async def cache_channel_delete(self, data):
-        await self.db.channels.delete_one({"_id": data["id"]})
+    def cache_channel_delete(self, data):
+        yield "channels", DeleteOne({"_id": data["id"]})
 
-    async def cache_guild_role_create(self, data):
+    def cache_guild_role_create(self, data):
         role = data["role"]
         role["_id"] = role["id"]
         role["guild_id"] = data["guild_id"]
-        await self.db.roles.update_one({"_id": role["id"]}, {"$set": role}, upsert=True)
+        yield "roles", UpdateOne({"_id": role["id"]}, {"$set": role}, upsert=True)
 
     def cache_guild_role_update(self, data):
-        return self.cache_guild_role_create(data)
+        yield from self.cache_guild_role_create(data)
 
-    async def cache_guild_role_delete(self, data):
-        await self.db.roles.delete_one({"_id": data["role_id"]})
+    def cache_guild_role_delete(self, data):
+        yield "roles", DeleteOne({"_id": data["role_id"]})
 
-    async def cache_guild_member_add(self, data):
-        await self.db.members.update_one({
+    def cache_guild_member_add(self, data):
+        yield "members", UpdateOne({
             # user.id and guild_id should be a unique compound index
             "id": data["user"]["id"],
             "guild_id": data["guild_id"]
         }, {"$set": data}, upsert=True)
 
     def cache_guild_member_update(self, data):
-        return self.cache_guild_member_add(data)
+        yield from self.cache_guild_member_add(data)
 
-    async def cache_guild_member_remove(self, data):
-        await self.db.members.delete_one({"id": data["user"]["id"], "guild_id": data["guild_id"]})
+    def cache_guild_member_remove(self, data):
+        yield "members", DeleteOne({"id": data["user"]["id"], "guild_id": data["guild_id"]})
 
     async def start(self):
         try:
             self.r_con = await aio_pika.connect_robust(self.rabbit_url)
             self.r_channel = await self.r_con.channel()
-            await self.r_channel.set_qos(prefetch_count=100)
+            await self.r_channel.set_qos(prefetch_count=self.max_bulk)
             await self.db.members.create_index("guild_id")
             await self.db.roles.create_index("guild_id")
             await self.db.channels.create_index("guild_id")
@@ -119,10 +174,8 @@ class Cacher:
             )
 
             queue = await self.r_channel.declare_queue("cache", arguments={"x-max-length": 10000})
-            async with queue.iterator() as messages:
-                async for msg in messages:
-                    async with msg.process():
-                        await self._message_received(msg)
+            await queue.consume(self._message_received)
+            self.loop.create_task(self.write_task())
 
         except ConnectionError:
             traceback.print_exc()
@@ -130,4 +183,5 @@ class Cacher:
             return await self.start()
 
     def run(self):
-        self.loop.run_until_complete(self.start())
+        self.loop.create_task(self.start())
+        self.loop.run_forever()
